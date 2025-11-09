e058ae3183b413420e7aa510327841a6d49bcd18
Arghya Malakar <arghyaapply2016@gmail.com>
2025-11-09 13:31:06 +0530

    cors check

--- app/main.py
+++ app/main.py
@@ -1,27 +1,3 @@
-# from fastapi import FastAPI, BackgroundTasks, HTTPException, Request, Depends
-# from fastapi.responses import HTMLResponse
-# from fastapi.middleware.cors import CORSMiddleware
-# from fastapi.templating import Jinja2Templates
-# from typing import List
-
-
-# app = FastAPI()
-
-# templates = Jinja2Templates(directory="app/templates")
-
-# app.add_middleware( 
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-# @app.get('/', response_class=HTMLResponse)
-# def index(request: Request):
-#     return templates.TemplateResponse("index.html", {"request": request})
-
-
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, StreamingResponse, JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
@@ -36,12 +12,20 @@ import zipfile
 app = FastAPI()
 templates = Jinja2Templates(directory="app/templates")
 
-app.add_middleware(
+
+app.add_middleware( 
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_methods=["*"],
+#     allow_headers=["*"]
+# )
 
 @app.get("/", response_class=HTMLResponse)
 def index(request: Request):
